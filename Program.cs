3454edc0b808a88ff0ab218f227413a379360bee
johan <johan@DESKTOP-CVTSQGU>
2022-08-29 14:48:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace githubPrac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej jag heter johan");
+        }
+    }
+}
